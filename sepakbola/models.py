dabc184d4b1dbbd85509eae449abfa90db2957aa
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-05-06 10:47:02 +0700

    Added model and form sketch. TODO: Create new user roles

--- sepakbola/models.py
+++ sepakbola/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    is_manajer = models.BooleanField(default=False)
+    is_penonton = models.BooleanField(default=False)
+    is_panitia = models.BooleanField(default=False)
