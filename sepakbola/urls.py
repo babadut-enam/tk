2c2d2b3fc49c374eacd47ecee7e4a5856fa780fa
Arya Daniswara Khairan <arya.daniswara@ui.ac.id>
2023-05-08 19:16:55 +0700

    bagian dk

--- sepakbola/urls.py
+++ sepakbola/urls.py
@@ -4,6 +4,8 @@ from sepakbola.views import show_manage_pertandingan, show_peristiwa_tim, show_p
 from sepakbola.views import register_manajer_penonton, register_panitia, show_list_pertandingan
 from sepakbola.views import show_history_rapat, show_pilih_pelatih, show_pilih_pemain
 from sepakbola.views import show_history_rapat, show_dashboard_manajer
+from sepakbola.views import form_isi_rapat, mulai_rapat, mulai_pertandingan, tim1_peristiwa, tim2_peristiwa
+from sepakbola.views import pembelian_tiket, pembelian_tiket2, pembelian_tiket3, pembelian_tiket4
 from django.urls import path
 
 app_name = 'sepakbola'
@@ -30,4 +32,14 @@ urlpatterns = [
     # path('manage-pertandingan/peristiwa-tim/<uuid:id>/', show_manage_pertandingan, name='peristiwa-tim'), TODO: ini yang bener tapi belum ada data jadi pake yang atas
     path('pembuatan-pertandingan/form-pemilihan-jadwal/', show_form_pemilihan_jadwal, name='form-pemilihan-jadwal'),
     path('pembuatan-pertandingan/form-pembuatan-pertandingan/', show_form_pembuatan_pertandingan, name='form-pembuatan-pertandingan'),
+    path('form-isi-rapat/', form_isi_rapat, name='form_isi_rapat'),
+    path('mulai-rapat/', mulai_rapat, name='mulai_rapat'),
+    path('mulai-pertandingan/', mulai_pertandingan, name='mulai_pertandingan'),
+    path('tim1-peristiwa/', tim1_peristiwa, name='tim1_peristiwa'),
+    path('tim2-peristiwa/', tim2_peristiwa, name='tim2_peristiwa'),
+    path('pembelian-tiket-stadium/', pembelian_tiket, name='pembelian_tiket'),
+    path('pembelian-tiket-waktu/', pembelian_tiket2, name='pembelian_tiket2'),
+    path('pembelian-tiket-vs/', pembelian_tiket3, name='pembelian_tiket3'),
+    path('pembelian-tiket-checkout/', pembelian_tiket4, name='pembelian_tiket4'),
+
 ]
